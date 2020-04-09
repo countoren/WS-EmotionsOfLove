2a34b6e693e7456edca18a4a8c04368dae360454
Oren Rozen <countoren@gmail.com>
2020-04-09 16:18:11 -0500

    styling images and navbar options

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -85,53 +85,57 @@ let button txt onClick =
 let navbarBotton name = 
     Navbar.Item.a [ Navbar.Item.Option.Props [ Style [ FontSize "larger" ]]] [ str name ]
 
+let sideImage imageUrl = 
+    Image.image [] [ img [ Src imageUrl; Style [ Height "100vh"] ] ]
 
 let view (model: Model) (dispatch: Msg -> unit) =
     div [ Style [ FontFamily "orpheus-pro" ] ]
         [ Navbar.navbar [ Navbar.Color IsWhite ]
+           [ 
+             Navbar.Item.div
+               [ Navbar.Item.Option.Props
+                   [ Style [ FontFamily "orpheus-pro"; FontSize "xx-large" ] ] 
+               ] [ str "EmotionsOfLove" ]
+             Navbar.Item.div [] []
+             navbarBotton "Home"
+             navbarBotton "About"
+             navbarBotton "Destiny"
+             navbarBotton "Sessions"
+             navbarBotton "Contact Me"
+           ]; 
+           Tile.ancestor [ Tile.Option.Props [ Style [ 
+               BackgroundImage "url(/images/background.jpg)"
+               BackgroundSize "cover"  
+            //    Height "100vh"
+            //    OverflowStyle OverflowOptions.Auto 
+              ] ] ]
               [ 
-                Navbar.Item.div
-                  [ Navbar.Item.Option.Props
-                      [ Style [ FontFamily "orpheus-pro"; FontSize "xx-large" ] ] 
-                  ] [ str "EmotionsOfLove" ]
-                Navbar.Item.div [] []
-                navbarBotton "Home"
-                navbarBotton "About"
-                navbarBotton "Destiny"
-                navbarBotton "Sessions"
-                navbarBotton "Contact Me"
-              ]
-
-          Tile.ancestor [ Tile.Option.Props [ Style [ BackgroundImage "url(/images/background.jpg)"; BackgroundSize "cover" ] ] ]
-              [ 
-              Tile.parent [ Tile.Modifiers [ Modifier.IsHidden (Screen.Mobile, true)] ] [
-                  Tile.child [] [ Image.image [] [ img [ Src "/images/ManHead320.png" ] ] ]
-               ]
-              Tile.parent [ Tile.Size Tile.Is7; Tile.IsVertical] [
+                Tile.parent [ Tile.Modifiers [ Modifier.IsHidden (Screen.Mobile, true)] ] [
+                  Tile.child [] [ sideImage "/images/ManHead320.png" ]
+                ]
+                Tile.parent [ Tile.Size Tile.Is7; Tile.IsVertical] [
                   Tile.child [] []
                   Tile.child [ Tile.Option.Props [ Style [ PaddingLeft "7%"; PaddingRight "7%"]]] 
                       [ Box.box' [ Props [ Style
                         [ BackgroundColor "rgba(255, 255, 255, 0.61)"
                           FontSize "larger" 
                         ] ] ]
-                      ]
                       [ str """Hi, my name is Katie and I am an Emotion Code Practitioner.
                           I would love to help you release trapped emotions so you can make space for Love in your life.
                           """
-                                br []
-                                br []
-                                str """
+                        br []
+                        br []
+                        str """
                           If you would like to learn more about this technique please click on About for detailed information. I look foward to hearing from you.
                           """ ]
                       ]
                   Tile.child [] []
-              ]
+                ]
 
-              Tile.parent [ ] [
-                  Tile.child [ ] [ Image.image [] [ img [ Src "/images/WomanHead320.png" ] ] ]
-               ]
-          ]
+                Tile.parent [ ] [ Tile.child [ ] [ sideImage "/images/WomanHead320.png" ] ]
+         ]
         ]
+        
 
 #if DEBUG
 open Elmish.Debug
